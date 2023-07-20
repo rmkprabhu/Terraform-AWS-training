d7ecde50e9448cc51e3851486393a94e91ff383d
rmkprabhu <manikandaprabhuravi@gmail.com>
2023-07-20 09:03:59 +0530

    Create Terraform-AWS-S3.tf

---
+++ Terraform-AWS-S3.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demos3" {
+    bucket = "${var.bucket_name}"
+}
